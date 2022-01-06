d21edca198e98b58c153c3ca6284015b72a4df19
David Vargas <dvarrui@protonmail.com>
2022-01-06 12:57:18 +0000

    U

--- episode/01-programing-with-nothing/07-booleans.rb
+++ episode/01-programing-with-nothing/07-booleans.rb
@@ -1,15 +1,22 @@
 #!/usr/bin/env ruby
 
-def true(x, y)
-  x
-end
+#def true(x, y)
+#  x
+#end
 
-def false(x, y)
-  y
-end
+#def false(x, y)
+#  y
+#end
 
 _TRUE  = -> x { -> y { x }}
 _FALSE = -> x { -> y { y }}
 
 puts _TRUE.(42).('hola')
 puts _FALSE.(42).('hola')
+
+def to_boolean(proc)
+  proc[true][false]
+end
+
+puts to_boolean(_TRUE)
+puts to_boolean(_FALSE)
---
+++ episode/01-programing-with-nothing/08-if.rb
@@ -0,0 +1,35 @@
+#!/usr/bin/env ruby
+
+ZERO    = -> p { -> x {           x      }}
+ONE     = -> p { -> x {         p[x]     }}
+TWO     = -> p { -> x {       p[p[x]]    }}
+THREE   = -> p { -> x {     p[p[p[x]]]   }}
+FOUR    = -> p { -> x {   p[p[p[p[x]]]]  }}
+FIVE    = -> p { -> x { p[p[p[p[p[x]]]]] }}
+FIFTEEN = -> p { -> x { p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[x]]]]]]]]]]]]]]] }}
+
+def to_integer(proc)
+  proc[ -> n {n + 1}][0]
+end
+
+_TRUE  = -> x { -> y { x }}
+_FALSE = -> x { -> y { y }}
+IF     = -> b { b }
+
+def to_boolean(proc)
+  IF[proc][true][false]
+end
+
+
+(to_integer(ONE)..to_integer(FIFTEEN)).map do |n|
+  IF[(n % to_integer(FIFTEEN)).zero?][
+    puts 'FizzBuzz'
+  ][IF[(n % to_integer(THREE)).zero?][
+    puts 'Fizz'
+
+  ][IF[(n % to_integer(FIVE)).zero?][
+    puts 'Buzz'
+  ][
+    puts n.to_s
+  ]]]
+end
