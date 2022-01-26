0614b8fef883b2d25dbe9432fa7ce598bfe87f4d
David Vargas <dvarrui@protonmail.com>
2022-01-26 00:33:26 +0000

    U

--- episode/01.introduccion/02.color/02-emojis.rb
+++ episode/01.introduccion/02.color/02-emojis.rb
@@ -1,21 +1,12 @@
 #!/usr/bin/env ruby
 # emoji URL: https://www.mclibre.org/consultar/htmlcss/html/html-unicode-secuencias-generos.html
 
-def example00
-  ##
-  # EXAMPLE 00: var names are UTF
-  puts "[Example 01] Nombres de variables en UTF"
-  # apl functional symbol alpha: U+237A Hex: &#x237a; Dec: &#9082;
-  ⍺ = 2
-  # circled number sixteen: U+246F Hex: &#x246f; Dec: &#9327;
-  ⑯ = 16
-  puts " => Tenemos que (\u{237a} * \u{246f} ) = #{(⍺ * ⑯)}"
-end
+require 'colorize'
 
 def example01
   ##
   # EXAMPLE 01: var names are UTF
-  puts "\n[Example 01] Nombres de variables en UTF"
+  puts "\n[Example 01] Nombres de variables en UTF".white
   # U+1F9D1 U+200D U+1F4BB # Hex: &#x1f9d1;&#x200d;&#x1f4bb;
   🧑‍💻 = "David"
   puts " => Hola! Soy #{🧑‍💻.to_s}. ¿Cómo te llamas?"
@@ -24,10 +15,9 @@ end
 def example02
   ##
   # EXAMPLE 02: puts UTF
-  puts "\n[Example 02] Ejemplo de impresión de caracteres UTF"
+  puts "\n[Example 02] Ejemplo de impresión de caracteres UTF".white
   puts " => \u{1f9d1} \u{2764} \u{1F48E}"
 end
 
-# example00
 example01
 example02
