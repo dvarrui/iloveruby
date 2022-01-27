cae1b74c0f5f3aca8acd2f320e931aa457c22b99
dvarrui <dvarrui@protonmail.com>
2022-01-27 16:53:38 +0000

    D

--- episode/etc/REVISE/21-des-ofuscar/02-desofuscar.rb
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env ruby
-
-require_relative '02-lib-desofuscar'
-
-Desofuscador.run(ARGV[0])
--- episode/etc/REVISE/27-maze-code-game/lib/maze-code-game/application.rb
+++
@@ -1,6 +0,0 @@
-##
-# Global application params
-module Application
-  NAME = 'MazeCodeGame'
-  VERSION = '0.0.0'
-end
