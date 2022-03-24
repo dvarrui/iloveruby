a90485481950011e6fc961d763a5fee0e6aafd79
David Vargas <dvarrui@protonmail.com>
2022-03-24 10:11:08 +0000

    [N] vagrant.

---
+++ episode/03.scripting/vagrant/ubuntu/install-software.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+apt update
+apt install -y vim tree nmap
+apt install -y git
+
+apt install -y figlet
+figlet ubuntu > /etc/motd
+echo "" >> /etc/motd
+echo "David Vargas Ruiz" >> /etc/motd
+echo "https://github.com/dvarrui" >> /etc/motd
+
+echo "# Adding more alias" >> /home/vagrant/.bashrc
+echo "alias c='clear'" >> /home/vagrant/.bashrc
+echo "alias v='vdir'" >> /home/vagrant/.bashrc
+
+lsb_release -d
