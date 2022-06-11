c44ad1efd09afa61ffbacd8d16f6ea190bf2cb6c
David Vargas <dvarrui@protonmail.com>
2022-06-11 12:05:25 +0100

    [F] .

--- online.sesions/sesion.04/03-taberna.rb
+++ online.sesions/sesion.04/03-taberna.rb
@@ -3,33 +3,38 @@
 def roll_dice(value)
   max = value + 1
   puts "Tiras el dado"
-  sleep 2
+  espera
 
   dice = rand(max)
-  while dado.zero?
-    dice = rando(max)
+  while dice.zero?
+    dice = rand(max)
     puts "¡Ha salido un #{dice}!"
-    sleep 2
+    espera
   end
-  puts "¡Ha salido un #{dado}!"
-  sleep 1
+  puts "¡Ha salido un #{dice}!"
+  espera
+  return dice
+end
+
+def espera(value=3)
+  #sleep value
 end
 
 def despertar
-  puts "*Despiertas en una taberna, te encuentras tirado en el suelo, con dolor de cabeza." \
+  puts "* Despiertas en una taberna, te encuentras tirado en el suelo, con dolor de cabeza." \
        "A primera vista no hay nadie más.*"
-  sleep 1
+  espera 3
   puts "*Pero al incorporporarte ves un hombre más alto que tú, te fijas en la" \
        "forma puntiaguda de las orejas, es el tabernero, es un elfo, está detrás " \
        "de la barra, dando la espalda.*"
-	sleep 2
+  espera 3
 
   puts "¿Qué haces?"
   puts "1. Te acercas a preguntarle qué ha pasado."
   puts "2. Intentas salir por la puerta sin hacer ruido. " \
        "(Dado de sigilo D20 - Para �xito se requiere una tirada de 10 o superior)"
 
-  gets.to_i
+  option = gets.to_i
 
   preguntar_que_ocurre if option == 1
   intentar_salir if option == 2
@@ -37,10 +42,10 @@ end
 
 def preguntar_que_ocurre
   puts "*Al acercarte llamas su atención.*"
-  sleep 1
+  espera 1
   puts "Tabernero: Buenos días dormilón, tras lo de anoche tuve que tomarte el pulso." \
        " Pensé que después de tal golpe en tu estado habrías muerto."
-  sleep 1
+  espera 1
 end
 
 def intentar_salir
@@ -55,24 +60,23 @@ end
 def el_tabernero_responde
   puts "*Mientras te vas acercando a la puerta, escuchas unos golpecitos en la barra" \
        ", al darte la vuelta, ves al tabernero observándote, con cara de sorpresa.*"
-  sleep 1
+  espera
   puts "Tabernero: Despiertas en el suelo, solo ves al dueño del local de espaldas " \
        "y lo primero que haces es intentar irte sin decir ni mu..."
-  sleep 1
+  espera
   puts "Tabernero: Ven aquí, acércate. Voy a intentar refrescarte lo ocurrido anoche..."
-  sleep 1
+  espera
   puts "*Te diriges a la barra. Donde te está esperando limpiándola*"
-  sleep 2
+  espera
 end
 
 def ir_a_la_puerta
   puts "*Al intentas ir hacia la puerta, uno de tus pies choca con una jarra de cerveza" \
        " derramada en el suelo, el tabernero lo escucha y se gira*"
-  sleep 1
+  espera
   puts "Tabernero: ¡Eh! ¿A dónde vas? Lo minimo es saludar, ven aquí"\
        ", que llevas horas ahí tirado."
-  sleep 1
+  espera
 end
 
 despertar
-
