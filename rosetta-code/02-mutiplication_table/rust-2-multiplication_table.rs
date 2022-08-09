2c335f19f96300356e3a9a41b7ab2a3f12ee1789
David Vargas <dvarrui@protonmail.com>
2022-08-09 16:53:29 +0100

    [U] .

--- rosetta-code/02-mutiplication_table/rust-2-multiplication_table.rs
+++ rosetta-code/02-mutiplication_table/rust-2-multiplication_table.rs
@@ -1,13 +1,14 @@
 fn main() {
     let xs = (1..=10)
         .map(|a| {
-            (1..=10).map(|b| {
-                if a > b {
-                    String::from("    ")
-                } else {
-                    format!("{:4}", a * b)
-                }
-            }).collect::<String>()
+            (1..=10)
+		.map(|b| {
+                	if a > b {
+                    	String::from("    ")
+                	} else {
+                    		format!("{:4}", a * b)
+                	}
+            	}).collect::<String>()
         }).collect::<Vec<String>>();
 
     println!("{}", xs.join("\n"))
