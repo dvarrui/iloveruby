7af796d12ea5ca818cca75578e060cc9c4e070db
David Vargas <dvarrui@protonmail.com>
2026-03-08 18:39:38 +0000

    rename

--- docs/01.inicio/02.aprender-con-ejemplos/04.listas/04-listas.rb
+++ docs/01.inicio/02.aprender-con-ejemplos/04.listas/04-listas.rb
@@ -8,17 +8,3 @@ puts "Escribir 10 números:"
   print("- numbers[#{_1}]? ")
   numbers[_1] = gets.to_i
 end
-
-# Motrar los números por pantalla
-puts "\nTenemos los siguientes valores:"
-# Calcular el total de ocurrencias de cada número
-occurrences = numbers.group_by { _1 }
-
-numbers.each do
-  # Mostrar el texto "NEGATIVO" cuando el número sea negativo
-  info1 = _1.negative? ? "(NEGATIVO)" : ""
-  # Mostrar el texto "REPETIDO" cuando las "ocurrencias" > 1
-  info2 = (occurrences[_1].size > 1) ? "(REPETIDO)" : ""
-
-  puts "- number #{_1} #{info1} #{info2}"
-end
