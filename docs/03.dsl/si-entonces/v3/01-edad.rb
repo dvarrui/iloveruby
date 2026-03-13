b2b9fd19513c6f9c00f039486a67975a2996a795
David Vargas <dvarrui@protonmail.com>
2026-03-13 13:48:16 +0000

    new v3 si-entonces-sino

---
+++ docs/03.dsl/si-entonces/v3/01-edad.rb
@@ -0,0 +1,18 @@
+#!/usr/bin/env ruby
+require_relative "lib/si-entonces-sino-v3"
+
+# Ejemplo 1: si-entonces
+edad = 10
+puts "\nEdad: #{edad}"
+
+si('edad >= 18', binding)
+  .entonces { puts "Adulto" }
+  .sino { puts "Menor" }
+
+# Ejemplo 2: sino
+edad = 55
+puts "\nEdad: #{edad}"
+
+si('edad >= 18', binding)
+  .entonces { puts "Adulto" }
+  .sino { puts "Menor" }
