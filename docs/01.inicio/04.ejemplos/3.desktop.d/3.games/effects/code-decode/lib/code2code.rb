951b4a3814542642771e6e877ecd2ac2675cb811
David Vargas <dvarrui@protonmail.com>
2026-03-15 14:54:10 +0000

    update

--- docs/01.inicio/04.ejemplos/3.desktop.d/3.games/effects/code-decode/lib/code2code.rb
+++ docs/01.inicio/04.ejemplos/3.desktop.d/3.games/effects/code-decode/lib/code2code.rb
@@ -5,6 +5,7 @@ class Code2Code
     @pastel = Pastel.new
     @filename = filename
     @input_lines = load(@filename)
+    @output_lines = []
   end
 
   def load(filename)
@@ -27,6 +28,22 @@ class Code2Code
   def show
     puts "==> show: "
     puts @pastel.blue.bold(@input_lines.join("\n").to_s)
+    return if @output_lines.empty?
+    puts @pastel.green.bold(@output_lines.join("\n").to_s)
+  end
+
+  def code
+    l1 = { a: "4", e: "3", i: "1", o: "0"}
+    l2 = { b: "8", q: "9", s: "5", t: "7"}
+    letters = l1.merge(l2)
+    # binding.break
+    @input_lines.each do |input_line|
+      output_line = input_line.dup
+      letters.each_pair do |from, to|
+        output_line = output_line.tr(from.to_s, to)
+      end
+      @output_lines << output_line
+    end
   end
 end
 
--- docs/01.inicio/04.ejemplos/3.desktop.d/3.games/effects/code-decode/main.rb
+++ docs/01.inicio/04.ejemplos/3.desktop.d/3.games/effects/code-decode/main.rb
@@ -1,5 +1,7 @@
 #!/usr/bin/env ruby
 require_relative "lib/code2code"
+require "debug"
 
 c2c = Code2Code.new(ARGV[0])
+c2c.code
 c2c.show
