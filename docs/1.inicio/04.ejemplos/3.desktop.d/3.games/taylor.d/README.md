4fc91d9ed655a3d78715c7419d6977551952a035
David Vargas <dvarrui@protonmail.com>
2026-08-20 11:26:28 +0100

    new hello game

---
+++ docs/1.inicio/04.ejemplos/3.desktop.d/3.games/taylor.d/README.md
@@ -0,0 +1,16 @@
+
+# Taylor
+
+Enlaces de interés:
+
+* Web site: https://taylormadetech.dev/
+* Repo: https://github.com/HellRok/Taylor
+
+# Instalación
+
+* Descargar el zip (https://taylormadetech.dev/)
+* Descomprimir y usar el comando `taylor`
+* `taylor new hello`, para crear el proyecto "hello".
+* Para ejecutar el juego.
+    * `taylor hello/game.rb`
+    * `cd hello && taylor`
---
+++ docs/1.inicio/04.ejemplos/3.desktop.d/3.games/taylor.d/hello/game.rb
@@ -0,0 +1,39 @@
+# Add the path ./vendor so we can easily require third party libraries.
+$: << './vendor'
+
+# Open up a window
+Window.open(width: 800, height: 480, title: "hello")
+
+# Setup audio so we can play sounds
+Audio.open
+
+# Get the current monitor frame rate and set our target frame rate to match.
+Window.target_frame_rate = Monitor.current.refresh_rate
+
+# Define your main method
+def main
+  # Get the amount of time passed since the last frame was rendered
+  delta = Window.frame_time
+
+  # Your update logic goes here
+
+  Window.draw do
+    # Your drawing logic goes here
+
+    Window.clear(colour: Colour::RAYWHITE)
+    Font.default.draw(
+      "Welcome to your first Taylor application!",
+      x: 190, y: 200, size: 20, colour: Colour::DARKGRAY
+    )
+  end
+end
+
+if Taylor::Platform.browser?
+  Browser.main_loop = "main"
+else
+  # Detect window close button or ESC key
+  main until Window.close?
+end
+
+Audio.close
+Window.close
